087c317336e6aff6e30024aec74fca0c90050a5f
yowi0 <daniofer0@gmail.com>
2024-05-09 10:21:41 +0200

    Nuevas clases Especie, Resultat

---
+++ plantid/src/main/java/plantid/Especie.java
@@ -0,0 +1,17 @@
+package plantid;
+
+public class Especie {
+	 private String scientificName;
+	 public Especie() 
+	 {
+		 
+	 }
+	 public String getScientificName() 
+	 {
+		 return scientificName;
+	 }
+	 public void setScientificName(String scientificName) 
+	 {
+		 this.scientificName = scientificName;
+	 }
+}
---
+++ plantid/src/main/java/plantid/Resultat.java
@@ -0,0 +1,32 @@
+package plantid;
+
+public class Resultat 
+{
+	private Double score;
+	private Especie species;
+	public Resultat() 
+	{
+
+	}
+	
+	public Double getScore() 
+	{
+		return score;
+	}
+	
+	public void setScore(Double score) 
+	{
+		this.score = score;
+	}
+	
+	public Especie getSpecies() 
+	{
+		return species;
+	}
+	
+	public void setSpecies(Especie species) 
+	{
+		this.species = species;
+	}
+}
+
